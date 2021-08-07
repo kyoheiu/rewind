fa08208bbbc5e64bfc348e13f44e7befd0b8014e
Kyohei Uto <kyoheiu@outlook.com>
2021-08-08 05:58:31 +0900

    Change message when no args

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,11 @@ import Text.Read (readMaybe)
 pkgPath :: FilePath
 pkgPath = "/var/cache/pacman/pkg"
 
-fstMessage = "Which package do you want to use?(Enter a number)"
+help = "A CLI tool to downgrade packages using local Pacman cache.\n\
+\`rewind package-name` will do it.\n\
+\`rewind name1 name2...` will also work."
+
+message = "Which package do you want to use?(Enter a number)"
 
 order = ["1) ","2) ","3) ","4) ","5) "]
 
@@ -24,11 +28,10 @@ findPkg needle heystack = T.isPrefixOf needle' heystack && not (T.isSuffixOf ".s
 listUp :: [String] -> [T.Text] -> [(String,[T.Text])]
 listUp as pkgs = map (\a -> (a, filter (findPkg a) pkgs)) as
 
-readInt x = readMaybe x :: Maybe Int
-
 getNum :: Int -> IO Int
 getNum n = getLine >>= \x ->
-  case readInt x of
+  let readInt x = readMaybe x :: Maybe Int
+  in case readInt x of
     Just x' -> if x' <= n then return x'
               else putStrLn "Invalid number. Re-enter a number." >> getNum n
     Nothing -> putStrLn "Invalid input. Re-enter a number." >> getNum n
@@ -38,15 +41,15 @@ downGrade result =
   if null (snd result) then putStrLn $ fst result ++ ": No such package."
   else
     let ordered = zipWith T.append order result' in
-    putStrLn fstMessage >> mapM_ TIO.putStrLn ordered >> getNum (length ordered) >>= \c ->
+    putStrLn message >> mapM_ TIO.putStrLn ordered >> getNum (length ordered) >>= \c ->
       doCommand $ result'!!(c-1)
         where result' = reverse . sort . snd $ result
-              doCommand x = callCommand . T.unpack 
+              doCommand x = callCommand . T.unpack
                           . T.append "sudo pacman -U /var/cache/pacman/pkg/" $ x
 
 main :: IO ()
 main = getArgs >>= \args ->
   case length args of
-    0 -> putStrLn "No args."
+    0 -> putStrLn help
     _ -> listDirectory pkgPath >>= \pkgs ->
       let results = listUp args (map T.pack pkgs) in mapM_ downGrade results
