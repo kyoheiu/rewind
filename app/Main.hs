863ac5fcd9f84798a04c94ac406690728a46375c
Kyohei Uto <kyoheiu@outlook.com>
2021-08-07 11:46:41 +0900

    Show packages up to 5 / Add package's name when error

--- app/Main.hs
+++ app/Main.hs
@@ -12,27 +12,31 @@ import System.Process
 pkgPath :: FilePath
 pkgPath = "/var/cache/pacman/pkg"
 
+fstMessage = "Which package do you want to use?(Enter a number)"
+
+order = ["1) ","2) ","3) ","4) ","5) "]
+
 findPkg :: String -> T.Text -> Bool
 findPkg needle heystack = T.isPrefixOf needle' heystack && not (T.isSuffixOf ".sig" heystack)
   where needle' = T.pack needle
 
-listUp :: [String] -> [FilePath] -> [[T.Text]]
+listUp :: [String] -> [FilePath] -> [(String,[T.Text])]
 listUp [] pkgs = []
-listUp (a:as) pkgs = filter (findPkg a) pkgs' : listUp as pkgs
+listUp (a:as) pkgs = (a, filter (findPkg a) pkgs') : listUp as pkgs
   where pkgs' = map T.pack pkgs
 
-downGrade :: [T.Text] -> IO ()
+downGrade :: (String, [T.Text]) -> IO ()
 downGrade result =
-  if null result then putStrLn "No such package."
+  if null (snd result) then putStrLn $ fst result ++ ": No such package." 
   else
-    let ordered = zipWith T.append ["1) ","2) ","3) "] . reverse . sort $ result in
-    putStrLn "Enter the number:" >> mapM_ TIO.putStrLn ordered >> readLn >>= \c ->
+    let ordered = zipWith T.append order result' in
+    putStrLn fstMessage >> mapM_ TIO.putStrLn ordered >> readLn >>= \c ->
       case c of
         1 -> doCommand $ result'!!0
         2 -> doCommand $ result'!!1
         3 -> doCommand $ result'!!2
-        _   -> putStrLn "Please Enter the number."
-        where result' = reverse . sort $ result
+        _ -> putStrLn "Please Enter a number."
+        where result' = reverse . sort . snd $ result
               doCommand x = callCommand . T.unpack 
                           . T.append "sudo pacman -U /var/cache/pacman/pkg/" $ x
 
